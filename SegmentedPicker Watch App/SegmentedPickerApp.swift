4b2ce7c1aad5ec1ed20d3ff56d06652599a666eb
Caleb McCreary <clsmccreary@gmail.com>
2024-04-13 04:00:09 -0700

    Initial Commit

---
+++ SegmentedPicker Watch App/SegmentedPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentedPickerApp.swift
+//  SegmentedPicker Watch App
+//
+//  Created by Caleb McCreary on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SegmentedPicker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
